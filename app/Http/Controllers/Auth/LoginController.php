440cedb40c804839864013ef3b18678bf8f62da2
januarelsan <januarelsan@gmail.com>
2021-02-25 01:35:23 +0700

    Add Redirect Function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,36 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Check user's role and redirect user based on their role
+     * @return 
+     */
+    public function authenticated()
+    {
+        // if(auth()->user()->hasRole('admin'))
+        // {
+        //     return redirect('/admin/dashboard');
+        // } 
+
+        return redirect('/');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        
+        $this->guard()->logout();
+ 
+        $request->session()->flush();
+ 
+        $request->session()->regenerate();
+        return redirect('home');
     }
 }
