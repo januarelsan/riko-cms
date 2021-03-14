5069f9433212d0628b5601b019c8e152d81dc802
januarelsan <januarelsan@gmail.com>
2021-03-15 00:42:15 +0700

    Add Timestamps

--- app/User.php
+++ app/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $primaryKey = 'id';
+    public $timestamps = true;
     /**
      * The attributes that are mass assignable.
      *
