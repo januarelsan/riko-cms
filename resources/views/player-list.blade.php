a01d7b1e1e438029236a14277605dba6b2d986d7
januarelsan <januarelsan@gmail.com>
2021-03-24 02:00:57 +0700

    Update View

--- resources/views/player-list.blade.php
+++ resources/views/player-list.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-5 col-8 align-self-center">
         <h3 class="text-themecolor m-b-0 m-t-0">Dashboard</h3>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="javascript:void(0)">Admin</a></li>
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Player</a></li>
             <li class="breadcrumb-item active">List</li>
         </ol>
     </div>                    
